655e7e4128b53656f949c8af9c719a5a6ca63ff5
Afif Sohaili (Ascenda) <afif.sohaili@kaligo.com>
2024-11-18 14:23:26 +0800

    Add function to get PR number and display PR and Jenkins URLs

--- github.zsh
+++ github.zsh
@@ -12,3 +12,12 @@ create_github_pr () {
   echo "Creating pull request at: $REMOTE/pull/new/$BRANCH_NAME"
   open $REMOTE/pull/new/$BRANCH_NAME
 }
+
+get_pr_number () {
+  BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+  local PR_NUMBER=`gh pr list --json headRefName,number --jq "map(select(.headRefName == \"$BRANCH_NAME\")) | .[0].number"`
+  local REMOTE=`git remote get-url --push origin | tr '[:upper:]' '[:lower:]' | sed 's/git@/https:\/\//;s/:kaligo\//\/Kaligo\//;s/\.git//'`
+  echo "PR: $REMOTE/pull/$PR_NUMBER"
+  local CURRENT_FOLDER=`pwd | sed 's/.*\///'`
+  echo "Jenkins: https://jenkins.int.kaligo.com/blue/organizations/jenkins/Ascenda%2F$CURRENT_FOLDER/activity?branch=PR-$PR_NUMBER"
+}
