c96918a818742544f6a27df93fdd36214d23b05a
Afif Sohaili (Ascenda) <afif.sohaili@kaligo.com>
2025-07-29 17:00:54 +0800

    Add alert function and update VS Code settings for Git blame and terminal

--- osascript.zsh
+++ osascript.zsh
@@ -1,3 +1,7 @@
 function notify() {
   osascript -e "display notification \"$1\" with title \"Notification\""
 }
+
+function alert() {
+  osascript -e "display dialog \"$1\" with title \"Alert\" buttons {\"Ok\"} default button \"Ok\""
+}
